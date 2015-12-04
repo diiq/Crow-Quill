e85cab60be6d4b0e142de025019c3363cc0bdc0d
Sam Bleckley <sam@mutuallyhuman.com>
2015-12-04 11:44:14 -0500

    Pre-synx directory restructuring

---
+++ Workspace/Workspace.swift
@@ -0,0 +1,9 @@
+/**
+ The Workspace consists of the drawing, the active drawing, any guides that are 
+ active, and any settings or state (like tool selection, etc)
+*/
+
+class Workspace {
+  let activeDrawing = ActiveDrawing()
+  
+}
